4062f6c653fe0585812b9feef468837542171c07
Sidharth K Shaji <sidharthkshaji@gmail.com>
2026-08-10 20:01:09 +0530

    Completed question 3

---
+++ question3.py
@@ -0,0 +1,14 @@
+number = int(input("Enter number:"))
+even_count = 0
+odd_count = 0
+for i in range(1,number+1):
+    if number*i % 2 == 0:
+        print(f"{number} x {i} = {number*i} - Even")
+        even_count += 1
+    else:
+        print(f"{number} x {i} = {number*i} - Odd")
+        odd_count += 1
+
+print("Even Results:",even_count)
+print("Odd Results:",odd_count)
+    
